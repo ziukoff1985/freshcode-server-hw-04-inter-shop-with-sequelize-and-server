7dedaa8ec27748f488f0667432260d5e8eccdf3b
Borys Ziukov <ziukoff1985@gmail.com>
2026-05-25 15:04:58 +0300

    Add validation for ItemCategories

--- src/routers/itemCategoriesRouter.js
+++ src/routers/itemCategoriesRouter.js
@@ -1,14 +1,24 @@
 const { Router } = require('express');
 // -------------------------------
 const itemCategoriesController = require('../controllers/itemCategoriesController');
+const { validateBody } = require('../middleware/index');
+const {
+    ITEM_CATEGORY_VALIDATION_SCHEMA,
+} = require('../utils/validationSchemas');
 // -------------------------------
 const router = new Router();
 
 router
     .route('/')
     .get(itemCategoriesController.getAllCategories)
-    .post(itemCategoriesController.createCategory)
-    .put(itemCategoriesController.updateCategory);
+    .post(
+        validateBody(ITEM_CATEGORY_VALIDATION_SCHEMA),
+        itemCategoriesController.createCategory,
+    )
+    .put(
+        validateBody(ITEM_CATEGORY_VALIDATION_SCHEMA),
+        itemCategoriesController.updateCategory,
+    );
 router
     .route('/:id')
     .get(itemCategoriesController.getCategoryById)
