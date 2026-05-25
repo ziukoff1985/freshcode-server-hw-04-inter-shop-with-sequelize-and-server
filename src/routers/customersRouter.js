66046233f8976a4606bbe5645dbca30cded12aef
Borys Ziukov <ziukoff1985@gmail.com>
2026-05-25 14:58:50 +0300

    Add validation for Customers

--- src/routers/customersRouter.js
+++ src/routers/customersRouter.js
@@ -1,15 +1,24 @@
 const { Router } = require('express');
 // -------------------------------
 const customersController = require('../controllers/customersController');
-const { hashPass } = require('../middleware/index');
+const { hashPass, validateBody } = require('../middleware/index');
+const { CUSTOMER_VALIDATION_SCHEMA } = require('../utils/validationSchemas');
 // -------------------------------
 const router = new Router();
 
 router
     .route('/')
     .get(customersController.getAllCustomers)
-    .post(hashPass.hashPassword, customersController.createCustomer)
-    .put(hashPass.hashPassword, customersController.updateCustomer);
+    .post(
+        validateBody(CUSTOMER_VALIDATION_SCHEMA),
+        hashPass.hashPassword,
+        customersController.createCustomer,
+    )
+    .put(
+        validateBody(CUSTOMER_VALIDATION_SCHEMA),
+        hashPass.hashPassword,
+        customersController.updateCustomer,
+    );
 router
     .route('/:id')
     .get(customersController.getCustomerById)
