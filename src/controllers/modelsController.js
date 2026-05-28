bd0cd7aee5f66d70b11fbb4a958707e46c425b84
Borys Ziukov <ziukoff1985@gmail.com>
2026-05-28 16:24:06 +0300

    Add specific controllers, roters and validation for models

--- src/controllers/modelsController.js
+++ src/controllers/modelsController.js
@@ -1,15 +1,20 @@
 const createError = require('http-errors');
+const { Op } = require('sequelize');
 
 const { Model, Brand } = require('../db/models/index');
+const { raw } = require('express');
 
 class ModelsController {
     async getAllModels(req, res, next) {
         try {
+            const { limit, offset } = req.pagination;
             const models = await Model.findAll({
-                limit: 10,
-                order: [['id', 'ASC']],
                 attributes: ['id', 'title', 'description'],
                 include: [{ model: Brand, attributes: ['title'] }],
+                raw: true,
+                limit,
+                offset,
+                order: [['id', 'ASC']],
             });
             if (models.length === 0) {
                 return next(createError(404, 'Models not found'));
@@ -29,12 +34,83 @@ class ModelsController {
                 where: { id },
                 attributes: ['id', 'title', 'description'],
                 include: [{ model: Brand, attributes: ['title'] }],
+                raw: true,
             });
             if (!model) {
                 return next(createError(404, 'Model not found'));
             }
             console.log(`Result is: ${JSON.stringify(model, null, 2)}`);
             res.status(200).json(model);
+        } catch (error) {
+            console.log(error.message);
+            next(error);
+        }
+    }
+
+    async getModelsFromHalf(req, res, next) {
+        try {
+            // get all ids from table
+            const allModels = await Model.findAll({
+                attributes: ['id'],
+                order: [['id', 'ASC']],
+                raw: true,
+            });
+
+            if (allModels.length === 0) {
+                return next(createError(404, 'Models not found'));
+            }
+
+            // find the index of the middle element of the array and get the actual ID from there.
+            const halfIndex = Math.floor(allModels.length / 2);
+            const targetId = allModels[halfIndex - 1].id;
+
+            // Select IDs that are greater than the average
+            const models = await Model.findAll({
+                attributes: ['id', 'title', 'description'],
+                include: [{ model: Brand, attributes: ['title'] }],
+                where: {
+                    id: {
+                        [Op.gt]: targetId,
+                    },
+                },
+                raw: true,
+                order: [['id', 'ASC']],
+            });
+
+            console.log(
+                `Result from half (id > ${targetId}) is: ${JSON.stringify(models, null, 2)}`,
+            );
+            res.status(200).json(models);
+        } catch (error) {
+            console.log(error.message);
+            next(error);
+        }
+    }
+
+    async getModelsByTitle(req, res, next) {
+        try {
+            const { values } = req.body;
+
+            if (!values || !Array.isArray(values) || values.length === 0) {
+                return next(createError(400, 'Brand titles are required'));
+            }
+
+            const models = await Model.findAll({
+                attributes: ['id', 'title', 'description'],
+                include: [{ model: Brand, attributes: ['title'] }],
+                where: {
+                    title: {
+                        [Op.in]: values,
+                    },
+                },
+                raw: true,
+                order: [['id', 'ASC']],
+            });
+            if (models.length === 0) {
+                return next(createError(404, 'Models not found'));
+            }
+            console.log(`Result is: ${JSON.stringify(models, null, 2)}`);
+            res.status(200).json(models);
         } catch (error) {
             console.log(error.message);
             next(error);
@@ -71,6 +147,36 @@ class ModelsController {
             console.log(`Deleted rows: ${deletedRows}`);
             res.status(200).json({
                 message: 'Model deleted successfully',
+            });
+        } catch (error) {
+            console.log(error.message);
+            next(error);
+        }
+    }
+
+    async deleteModelsByTitles(req, res, next) {
+        try {
+            const { values } = req.body;
+
+            if (!values || !Array.isArray(values) || values.length === 0) {
+                return next(createError(400, 'Model titles are required'));
+            }
+
+            const deletedRows = await Model.destroy({
+                where: {
+                    title: {
+                        [Op.in]: values,
+                    },
+                },
+            });
+
+            if (deletedRows === 0) {
+                return next(createError(404, 'Models not found'));
+            }
+
+            console.log(`Deleted rows: ${deletedRows}`);
+            res.status(200).json({
+                message: 'Models deleted successfully',
             });
         } catch (error) {
             console.log(error.message);
--- src/routers/modelsRouter.js
+++ src/routers/modelsRouter.js
@@ -1,16 +1,32 @@
 const { Router } = require('express');
 // -------------------------------
 const modelsController = require('../controllers/modelsController');
-const { validateBody } = require('../middleware/index');
-const { MODEL_VALIDATION_SCHEMA } = require('../utils/validationSchemas');
+const { validateBody, paginate } = require('../middleware/index');
+const {
+    MODEL_VALIDATION_SCHEMA,
+    BULK_FIND_SCHEMA,
+} = require('../utils/validationSchemas');
 // -------------------------------
 const router = new Router();
 
 router
     .route('/')
-    .get(modelsController.getAllModels)
+    .get(paginate.paginateElements, modelsController.getAllModels)
     .post(validateBody(MODEL_VALIDATION_SCHEMA), modelsController.createModel)
     .put(validateBody(MODEL_VALIDATION_SCHEMA), modelsController.updateModel);
+
+router.route('/half').get(modelsController.getModelsFromHalf);
+
+router
+    .route('/by-titles')
+    .post(validateBody(BULK_FIND_SCHEMA), modelsController.getModelsByTitle);
+
+router
+    .route('/del-by-titles')
+    .delete(
+        validateBody(BULK_FIND_SCHEMA),
+        modelsController.deleteModelsByTitles,
+    );
 router
     .route('/:id')
     .get(modelsController.getModelById)
