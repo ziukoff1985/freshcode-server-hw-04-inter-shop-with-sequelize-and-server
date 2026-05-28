1d2be748eacfe81a986303185469b131f8bbcc64
Borys Ziukov <ziukoff1985@gmail.com>
2026-05-28 19:33:11 +0300

    Add specific controllers, roters and validation for stores

--- src/controllers/storesController.js
+++ src/controllers/storesController.js
@@ -1,12 +1,16 @@
 const createError = require('http-errors');
+const { Op } = require('sequelize');
 
 const { Store } = require('../db/models/index');
 
 class StoresController {
     async getAllStores(req, res, next) {
         try {
+            const { limit, offset } = req.pagination;
             const stores = await Store.findAll({
-                limit: 10,
+                raw: true,
+                limit,
+                offset,
                 order: [['id', 'ASC']],
             });
             if (stores.length === 0) {
@@ -35,6 +39,69 @@ class StoresController {
         }
     }
 
+    async getStoresFromHalf(req, res, next) {
+        try {
+            // get all ids from table
+            const allStores = await Store.findAll({
+                attributes: ['id'],
+                order: [['id', 'ASC']],
+                raw: true,
+            });
+
+            if (allStores.length === 0) {
+                return next(createError(404, 'Stores not found'));
+            }
+
+            // find the index of the middle element of the array and get the actual ID from there.
+            const halfIndex = Math.floor(allStores.length / 2);
+            const targetId = allStores[halfIndex - 1].id;
+
+            // Select IDs that are greater than the average
+            const stores = await Store.findAll({
+                where: {
+                    id: {
+                        [Op.gt]: targetId,
+                    },
+                },
+                order: [['id', 'ASC']],
+            });
+            console.log(
+                `Result from half (id > ${targetId}) is: ${JSON.stringify(stores, null, 2)}`,
+            );
+            res.status(200).json(stores);
+        } catch (error) {
+            console.log(error.message);
+            next(error);
+        }
+    }
+
+    async getStoresByTitle(req, res, next) {
+        try {
+            const { values } = req.body;
+
+            if (!values || !Array.isArray(values) || values.length === 0) {
+                return next(createError(400, 'Store titles are required'));
+            }
+
+            const stores = await Store.findAll({
+                where: {
+                    title: {
+                        [Op.in]: values,
+                    },
+                },
+                order: [['id', 'ASC']],
+            });
+            if (stores.length === 0) {
+                return next(createError(404, 'Stores not found'));
+            }
+            console.log(`Result is: ${JSON.stringify(stores, null, 2)}`);
+            res.status(200).json(stores);
+        } catch (error) {
+            console.log(error.message);
+            next(error);
+        }
+    }
+
     async createStore(req, res, next) {
         try {
             const { title, description } = req.body;
@@ -57,6 +124,36 @@ class StoresController {
             console.log(`Deleted rows: ${deletedRows}`);
             res.status(200).json({
                 message: 'Store deleted successfully',
+            });
+        } catch (error) {
+            console.log(error.message);
+            next(error);
+        }
+    }
+
+    async deleteStoresByTitles(req, res, next) {
+        try {
+            const { values } = req.body;
+
+            if (!values || !Array.isArray(values) || values.length === 0) {
+                return next(createError(400, 'Store titles are required'));
+            }
+
+            const deletedRows = await Store.destroy({
+                where: {
+                    title: {
+                        [Op.in]: values,
+                    },
+                },
+            });
+
+            if (deletedRows === 0) {
+                return next(createError(404, 'Stores not found'));
+            }
+
+            console.log(`Deleted rows: ${deletedRows}`);
+            res.status(200).json({
+                message: 'Stores deleted successfully',
             });
         } catch (error) {
             console.log(error.message);
--- src/routers/storesRouter.js
+++ src/routers/storesRouter.js
@@ -1,16 +1,32 @@
 const { Router } = require('express');
 // -------------------------------
 const storesController = require('../controllers/storesController');
-const { validateBody } = require('../middleware/index');
-const { STORE_VALIDATION_SCHEMA } = require('../utils/validationSchemas');
+const { validateBody, paginate } = require('../middleware/index');
+const {
+    STORE_VALIDATION_SCHEMA,
+    BULK_FIND_SCHEMA,
+} = require('../utils/validationSchemas');
 // -------------------------------
 const router = new Router();
 
 router
     .route('/')
-    .get(storesController.getAllStores)
+    .get(paginate.paginateElements, storesController.getAllStores)
     .post(validateBody(STORE_VALIDATION_SCHEMA), storesController.createStore)
     .put(validateBody(STORE_VALIDATION_SCHEMA), storesController.updateStore);
+
+router.route('/half').get(storesController.getStoresFromHalf);
+
+router
+    .route('/by-titles')
+    .post(validateBody(BULK_FIND_SCHEMA), storesController.getStoresByTitle);
+
+router
+    .route('/del-by-titles')
+    .delete(
+        validateBody(BULK_FIND_SCHEMA),
+        storesController.deleteStoresByTitles,
+    );
 router
     .route('/:id')
     .get(storesController.getStoreById)
